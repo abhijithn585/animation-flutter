9fcb176e5a0d920d3baf80d5654ae8bb0e742592
abhijithn585 <abhijithn585@gmail.com>
2024-01-19 10:01:25 +0530

    first commit

---
+++ lib/constants/data.dart
@@ -0,0 +1,14 @@
+import 'package:animation/model/data_model.dart';
+
+List<DataModel> data = [
+DataModel(
+  id: '1s',
+  name:'Porche 911 GT3',
+  image: "assets/GT3.jpg",
+  description: '''jdk'''
+)
+DataModel(
+  id: '2s',
+  name: 'Sls Gullwing'
+)
+];
---
+++ lib/model/data_model.dart
@@ -0,0 +1,7 @@
+class DataModel {
+  String? id;
+  String? image;
+  String? name;
+  String? description;
+  DataModel({this.id, this.description, this.image,this.name});
+}
